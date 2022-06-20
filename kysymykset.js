e529f1bea5458e81bdcadd327433520bfc65f9da
Pete M <petri.r.mattila@helsinki.fi>
2022-06-20 16:46:53 +0300

    Korjattu kysymykset.json testausta varten

--- kysymykset.js
+++ kysymykset.js
@@ -1,15 +1,13 @@
-{
-  Kysymykset: [
-    {
-      kysymys: "Kysymys 1",
-      vaihtoehdot: ["Vaihtoehto 1", "Vaihtoehto 2", "Vaihtoehto 3"],
-      oikeav: "Vaihtoehto 1",
-    },
+[
+  {
+    "kysymys": "Kysymys 1",
+    "vaihtoehdot": ["Vaihtoehto 1", "Vaihtoehto 2", "Vaihtoehto 3"],
+    "oikeav": "Vaihtoehto 1"
+  },
 
-    {
-      kysymys: "Kysymys 2",
-      vaihtoehdot: ["Vaihtoehto 1", "Vaihtoehto 2", "Vaihtoehto 3"],
-      oikeav: "Vaihtoehto 3",
-    },
-  ];
-}
+  {
+    "kysymys": "Kysymys 2",
+    "vaihtoehdot": ["Vaihtoehto 1", "Vaihtoehto 2", "Vaihtoehto 3"],
+    "oikeav": "Vaihtoehto 3"
+  }
+]
