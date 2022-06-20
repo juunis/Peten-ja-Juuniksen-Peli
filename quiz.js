c4bca2ad6e30e09bc6bc732777f1bfab21338973
juunis <juunisalihassan@gmail.com>
2022-06-20 15:58:37 +0300

    Started building loops

--- quiz.js
+++ quiz.js
@@ -1,1 +1,23 @@
-"Hello World";
+// QUIZ GAME
+
+// For user inputs -> npm install prompt-sync
+
+const prompt = require("prompt-sync")({ sigint: true });
+const answer = prompt("WELCOME TO OUR QUIZ! DO YOU WANT TO PLAY? ");
+
+if answer === "yes" {
+  let score = 0;
+  let question = 1;
+    let question1 = prompt("QUESTION 1:  A, B, C, or D? ");
+    if (question1 === "B") {
+      score++;
+    }
+}
+
+
+
+
+
+
+console.log("You scored " + score + " out of " + question + " questions!");
+
