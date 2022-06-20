c5cca000896dcbfa6e005ff152177638f6b68a44
juunis <juunisalihassan@gmail.com>
2022-06-20 17:18:16 +0300

    questions updated and in working order

--- quiz.js
+++ quiz.js
@@ -1,20 +1,22 @@
+"use strict";
 // QUIZ GAME
 
 // For user inputs -> npm install prompt-sync
 const kysymysLista = require("./kysymykset.json");
 const prompt = require("prompt-sync")({ sigint: true });
-const answer = prompt("HALUATKO PELATA? ");
+const answer = prompt("HALUATKO PELATA? joo/ei? ");
 
 if (answer === "joo") {
   let score = 0;
   for (let i = 0; i < kysymysLista.length; i++) {
-    let question = prompt(`${kysymysLista[i].kysymys}
-    Vaihtoehdot: ${kysymysLista[i].vaihtoehdot}`);
+    console.log(`${kysymysLista[i].kysymys}
+    Vastausvaihtoehdot: ${kysymysLista[i].vaihtoehdot}`);
+    let question = prompt(`Vastauksesi: `);
 
-    if (question === kysymysLista[i].oikeav) {
+    if (question.toUpperCase() === kysymysLista[i].oikeav) {
       score++;
-      console.log(`Vastasit oikein!`);
-    } else console.log(`Väärin meni!`);
+      console.log(`Vastasit oikein! \n`);
+    } else console.log(`Väärin meni! \n`);
   }
   console.log(`Sait ${score}/${[kysymysLista.length]}.`);
 } else {
